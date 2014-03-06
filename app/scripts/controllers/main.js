109d4a635be983928583f11b659e8fb8ffcd430c
Greg Roberts <gregorysroberts@gmail.com>
2014-03-05 20:09:02 -0700

    Adds div for park details

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,4 +4,8 @@ angular.module('oregonCoastApp')
   .controller('MainCtrl', function ($scope, parks) {
   	console.log('parks', parks);
     $scope.parks = parks;
+    $scope.selectedPark = parks[0];
+    $scope.setSelectedPark= function(park){
+    	setSelectedPark = park;
+    };
   });
