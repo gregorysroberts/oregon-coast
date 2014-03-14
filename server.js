2d990dae318ad82661d51c8f67387779164efcdb
Greg Roberts <gregorysroberts@gmail.com>
2014-03-13 20:58:43 -0600

    Sends the appropriate latitude and longitude to the server request, needs to fix the response to display properly

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.configure(function(){
 	app.use(function(req, res, next){
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, lat, lng');
 		res.type('application/json');
 		next();
 	})
@@ -17,7 +17,7 @@ app.configure(function(){
 
 app.get('/instagram', function(req, res) {
 
-	request("https://api.instagram.com/v1/media/search?lat=44.659645&lng=-124.056381&client_id=0a9cfb02cb1b4cd2899d4fa6a9c00c19", function(error, response, body) {
+	request("https://api.instagram.com/v1/media/search?lat=" + req.headers.lat + "&lng=" + req.headers.lng + "&client_id=0a9cfb02cb1b4cd2899d4fa6a9c00c19", function(error, response, body) {
   		res.json(JSON.parse(body));
 	});
 });
