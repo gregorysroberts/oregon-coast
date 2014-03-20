6991619b336abc55662ca8114be16900c69d9cae
Greg Roberts <gregorysroberts@gmail.com>
2014-03-19 21:10:00 -0600

    Scroll to top function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,22 +6,64 @@ angular.module('oregonCoastApp')
 
     $scope.selectedPark = parks[0];
 
-    $scope.setSelectedPark= function(park){
+    $scope.setSelectedPark= function(park, $location, $anchorScroll){
       $scope.selectedPark = park;
       PicturesService.getPictures(park.park_latitude, park.park_longitude).then(function(pictures) {
         $scope.pictures = pictures;
+
       });
 
       StateParksPictures.getStateParksPictures(park.park_id).then(function(statePictures) {
         $scope.statePictures = statePictures;
       });
+      $('html, body').animate({ scrollTop: 0 }, 0);
     };
 
     $scope.CarouselDemoCtrl = function($scope) {
       var slides = $scope.statePictures; 
     }
+
+    // $scope.ScrollCtrl = function ($scope, $location, $anchorScroll) {
+    //   $scope.gotoTop = function (){
+    //     // set the location.hash to the id of
+    //     // the element you wish to scroll to.
+    //     $location.hash('bottom');
+     
+    //     // call $anchorScroll()
+    //     $anchorScroll();
+    //   };
+    // }
+
+    // var ref = new Firebase('https://myfirstapp12345.firebaseio.com/');
+    //   $scope.messages = $firebase(ref);
+    //   $scope.addMessage = function(e){
+
+    //       //this checks to make sure that the keydown is on the enter button (#13)
+    //       if(e.keyCode !== 13){
+    //           return;
+    //       }
+    //       $scope.messages.$add({
+    //           body: $scope.newMessage
+    //       });
+    //       $scope.newMessage = '';
+    //   }
     
   });
 
-  
+  // angular.module('chatFirebaseApp', ['firebase'])
+  // .controller('MainCtrl', function ($scope, $firebase) {
+  // var ref = new Firebase('https://myfirstapp12345.firebaseio.com/');
+  //       $scope.messages = $firebase(ref);
+  //       $scope.addMessage = function(e){
 
+  //           //this checks to make sure that the keydown is on the enter button (#13)
+  //           if(e.keyCode !== 13){
+  //               return;
+  //           }
+  //           $scope.messages.$add({
+  //               body: $scope.newMessage
+  //           });
+  //           console.log("messages")
+  //           $scope.newMessage = '';
+  //       }
+  // });
