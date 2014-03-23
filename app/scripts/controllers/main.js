598a503b295a2bf7367bb97bd7c99f91e0cbd9e0
Greg Roberts <gregorysroberts@gmail.com>
2014-03-22 21:59:34 -0600

    Updates firebase calls to automatically appear on click

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('oregonCoastApp')
-  .controller('MainCtrl', function ($scope, parks, PicturesService, StateParksPictures, ParksTipService, $stateParams, $firebase) {
+  .controller('MainCtrl', function ($scope, parks, PicturesService, StateParksPictures, $stateParams, $firebase) {
     $scope.parks = parks;
 
     $scope.setSelectedPark= function(park){
@@ -15,10 +15,29 @@ angular.module('oregonCoastApp')
         $scope.statePictures = statePictures;
       });
 
-      ParksTipService.getParksTips(park.park_id).then(function(parkTips) {
-        $scope.parkTips = parkTips;
-      });
+      // ParksTipService.getParksTips(park.park_id).then(function(parkTips) {
+      //   $scope.parkTips = parkTips;
+      //   console.log(parkTips);
+      // });
 
+
+        var ref = new Firebase('https://oregoncoast.firebaseio.com/' + $scope.selectedPark.park_id);
+        $scope.parkMessages = $firebase(ref);
+      
+      $scope.addMessage = function(e){
+
+          //this checks to make sure that the keydown is on the enter button (#13)
+          if(e.keyCode !== 13){
+              return;
+          }
+
+          var ref = new Firebase('https://oregoncoast.firebaseio.com/' + $scope.selectedPark.park_id);
+
+          $scope.parkMessages.$add({
+                body: $scope.newMessage
+          });
+          $scope.newMessage = '';
+      }
 
       $('html, body').animate({ scrollTop: 0 }, 0);
 
@@ -30,21 +49,6 @@ angular.module('oregonCoastApp')
 
     $scope.setSelectedPark(parks[0]);
 
-      
-      $scope.addMessage = function(e){
 
-          //this checks to make sure that the keydown is on the enter button (#13)
-          if(e.keyCode !== 13){
-              return;
-          }
-
-          var ref = new Firebase('https://oregoncoast.firebaseio.com/' + $scope.selectedPark.park_id);
-          $scope.parkMessages = $firebase(ref);
-
-          $scope.parkMessages.$add({
-                body: $scope.newMessage
-          });
-          $scope.newMessage = '';
-      }
     
   });
